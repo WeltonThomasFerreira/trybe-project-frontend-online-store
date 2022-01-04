163b6c6b9657db696ffed20d8824368a95d31cc0
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-03 22:46:27 -0300

    Updates

---
+++ precache-manifest.17e9509c3c309dde6cb8aca973449de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce1e8f39252375bcdd6bbf7419570e9",
+    "url": "/trybe-project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "e414f821e698f51f2ade",
+    "url": "/trybe-project-frontend-online-store/static/css/main.2e1f2230.chunk.css"
+  },
+  {
+    "revision": "77242ba2f68937dadfa5",
+    "url": "/trybe-project-frontend-online-store/static/js/2.5a8ead05.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/trybe-project-frontend-online-store/static/js/2.5a8ead05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e414f821e698f51f2ade",
+    "url": "/trybe-project-frontend-online-store/static/js/main.d3c1bae3.chunk.js"
+  },
+  {
+    "revision": "d2f04ad9a5664c8743e7",
+    "url": "/trybe-project-frontend-online-store/static/js/runtime-main.23ec2c28.js"
+  }
+]);
